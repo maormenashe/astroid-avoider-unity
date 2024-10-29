1fd614ac19d9a1f611e473868b00bd329f49b7a0
MaorM <maormenashe2@gmailcom>
2024-10-29 22:47:57 +0200

    Spaceship Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine.InputSystem.Controls;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelocity;
+
     private Camera mainCamera;
+    private Rigidbody rb;
+
+    private Vector3 movementDirection;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -17,12 +24,27 @@ public class PlayerMovement : MonoBehaviour
 
         if (!primaryTouch.press.isPressed)
         {
+            movementDirection = Vector3.zero;
             return;
         }
 
         Vector2 touchPosition = primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log($"{touchPosition} {worldPosition}");
+        movementDirection = transform.position - worldPosition;
+        movementDirection.z = 0;
+        movementDirection.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementDirection == Vector3.zero)
+        {
+            return;
+        }
+
+        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+
+        rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVelocity);
     }
 }

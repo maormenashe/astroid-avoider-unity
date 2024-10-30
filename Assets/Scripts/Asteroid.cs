d3302d8fe743cabe6062f3336c6793cd7e8547bd
MaorM <maormenashe2@gmailcom>
2024-10-30 08:21:43 +0200

    Asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+        if (playerHealth == null)
+        {
+            return;
+        }
+
+        playerHealth.Crash();
+    }
+}

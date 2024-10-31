485c5ede77757e8c87fcd40df9259e6c31f59eee
MaorM <maormenashe2@gmailcom>
2024-10-31 09:02:11 +0200

    Main Menu UI & GameOver UI

---
+++ Assets/Scripts/GameOverHandler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverDisplay;
+    [SerializeField] private AsteroidSpawner asteroidSpawner;
+
+    public void EndGame()
+    {
+        asteroidSpawner.enabled = false;
+        gameOverDisplay.gameObject.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
